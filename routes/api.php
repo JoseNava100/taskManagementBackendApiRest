24f508bb50878829a9360847ff55fc139da4caeb
josenava <josenavaemanuel@outlook.es>
2024-11-11 15:25:00 -0700

    Api Controller Role CRUD in progress

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,28 @@
 <?php
 
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+    /**
+     * Run HTTP Request Task.
+     */
 
 Route::get('/task', [TaskController::class, 'index']);
 Route::post('/task', [TaskController::class, 'store']);
-Route::get('/task/{id}', [TaskController::class, 'show']);
-Route::patch('/task/{id}', [TaskController::class, 'edit']);
-Route::put('/task/{id}', [TaskController::class, 'update']);
-Route::delete('/task/{id}', [TaskController::class, 'destroy']);
+Route::get('/task/{task}', [TaskController::class, 'show']);
+Route::patch('/task/{task}', [TaskController::class, 'edit']);
+Route::put('/task/{task}', [TaskController::class, 'update']);
+Route::delete('/task/{task}', [TaskController::class, 'destroy']);
+
+    /**
+     * Run HTTP Request Role.
+     */
+    
+Route::get('/role', [RoleController::class, 'index']);
+Route::post('/role', [RoleController::class, 'store']);
+Route::get('/role/{role}', [RoleController::class, 'show']);
+Route::patch('/role/{role}', [RoleController::class, 'edit']);
+Route::put('/role/{role}', [RoleController::class, 'update']);
+Route::delete('/role/{role}', [RoleController::class, 'destroy']);
